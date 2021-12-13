57f6b04c6cec5d148498662d9c75274f346bd776
yasir19 <yasir.habboo@gmail.com>
2021-12-12 22:45:34 -0600

    working app

--- Develop/script.js
+++ Develop/script.js
@@ -1,87 +1,63 @@
-// MonthName array
-var monthName = ["January", "February", "March", "April", "May", "June",
-"July", "August", "September", "October", "November", "December"];
-// dayName Array 
-var dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-// dates array
-var dates =["1st", "2ed","3th","4th","5th","6th","7th","8th","9th","10th","11th",
-"12th","13th","14th","15th","16th","17th","18th","19th","20th","21st", "22ed","23th"
-,"24th","25th","26th","27th","28th","29th","30th","31st" ];
-// get today's date
-var today = new Date();
-// format the date
-var date = dayName[today.getDay()] + "," + monthName[today.getMonth()] + "," + dates[today.getDate()];
-// using jquery selectors to get the currentDay Id from html and set the content of thr date;
-$("#currentDay").text(date);
-var time = today.getHours();
-if (time > 12){
-    time -= 12;
-}else if (time===0){
-    time=12;
-}
-var AMPM = time >=12 ? 'PM':'AM';
-newTime = time+ AMPM;
-console.log(newTime);
-var arr=["10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
-for (var i=0;i<arr.length;i++){
-console.log(arr[i]);
-}
-var creatTask = ()=> {
-for (var i =0 ; i < 9; i++){
- var timeEl=$("<div>").addClass("p-4 border-top col-lg-2 time-block").text(arr[i]);
- var taskEl=$("<textarea>").addClass(" textarea p-4 border-top col-lg-8");
- var btnEl=$("<button>").addClass("saveBtn p-4 col-lg-2");;
-var rowsEl=$(".row").append(timeEl,taskEl,btnEl);
- $("#timeBlock").append(rowsEl);
-}
-}
-var currentTime = function(){
-    var getTime = newTime;
-    console.log(getTime);
-    $(".textarea").each(function(){
-        var hr =$("hours").text();
-        var eachHr =parseInt(hr);
-        if (eachHr < getTime){
+//gets today's date by using moment.js 
+var currentDay = moment().format('dddd, MMMM Do');
+//display the date on jumbotron by using jQuery API method
+$("#currentDay").text(currentDay);
+
+// check the time if it in the past/present/future
+function currentHour(){
+    // gets current hour by using moment.js API
+    var currentTime = moment().hour();
+    //get the time in the hour divs
+    $(".time-block").each(function(){
+        // gets the current timeblock's hour
+        var timeBlockHr = parseInt($(this).attr("id"));
+   
+        // check if the time in the past then give the element calss "past"
+        if(timeBlockHr < currentTime){
             $(this).addClass("past");
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-        }else if (eachHr === getTime ){
+        }
+        // check if the time in the present then give the element calss "present"
+        else if(timeBlockHr === currentTime){
             $(this).addClass("present");
-            $(this).removeClass("future");
-            $(this).removeClass("past");
-        }else{
+        }
+        // check if the time in the future then give the element calss "future"
+        else{
             $(this).addClass("future");
-            $(this).removeClass("past");
-            $(this).removeClass("present");
         }
     });
-}
 
-  
-    var loadTasks = function(){
-        tasks =JSON.parse(localStorage.getItem("tasks"));
-        if(!tasks){
-            tasks ={
-                task:[]
-            };
-        }
-    };
-    var saveTask= function(){
-        localStorage.setItem("tasks",JSON.stringify(tasks));
+
+};
+
+// loads the tasks 
+function loadTasks(){
+    // loop through the div by using the ID 
+    for(var i = 9; i < 18; i++){
+        // git the information basied on the id and loop over it, 
+        //then get the data from textarea that will be represented
+        // in localStorage 
+        $("#" + i + " .textarea").val(localStorage.getItem(i));
     }
-// $("#text").on("click","p", function(){
-//     var text=$(this)
-//     .text().trim();
-//     var textInput=$("<textarea>").addClass("textarea").vla(text);
-//     $(this).replaceWith(textInput)
-//     textInput.trigger("focus");
-// });
-$("#btn").on("click", "textarea", function(){
- localStorage.setItem("textInput",JSON.stringigy)
+};
 
+
+// when save button is clicked, saves the values within the button's div
+$(".saveBtn").on("click", function(){
+      // get the hour id from the div
+      var hour = $(this).parent().attr("id");
+       // get the value input in the textarea within that div
+       var Text = $(this).siblings(".textarea").val();
+    // sets them into local storage
+    localStorage.setItem(hour, Text);
 });
-loadTasks();
-currentTime();
-creatTask();
 
+// loads the tasks from within local storage
+loadTasks();
+
+// checks the hour for past/present/future
+currentHour();
+// make currentHour() happen every minute
+setInterval(function() {
+    currentHour();
+}, 60000);
 
---
+++ README.md
@@ -0,0 +1,26 @@
+###  Work Day Scheduler App
+## User Story 
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+## Acceptance Criteria
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+## App development 
+1- the App has been developed by using the third- web APIs such as jQuery, Bootstrap, and moment.js
+2- the App is responsive to multi-screen sizes (small, medium, and large)
+3- the app will highlight the past time with gray color, the present time with red, and the future colored in green 
+4- when the user press the save button the information will be stored locally 
+5- the App will check the time every minute to ensure time change. 
